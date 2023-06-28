cae96a1edff7221513a7a1072d64bfb382c50333
Vatsook1996 <ahmetdianovv@mail.ru>
2023-06-28 19:57:47 +0300

    init commit

---
+++ src/test/java/models/UserRegistrationResponse.java
@@ -0,0 +1,10 @@
+package models;
+
+import lombok.Data;
+
+@Data
+public class UserRegistrationResponse {
+
+    Integer id;
+    String token;
+}
---
+++ src/test/java/models/UsersIdCheckResponse.java
@@ -0,0 +1,11 @@
+package models;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.LinkedList;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UsersIdCheckResponse {
+    LinkedList<User> data;
+}
